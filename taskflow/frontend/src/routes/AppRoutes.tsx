4d301fe0cdd1af138dc5ed644f802e4c4789709c
Igor <igorvspedro73@gmail.com>
2025-10-13 11:34:51 -0300

    Criando a tela do dashboard

--- taskflow/frontend/src/routes/AppRoutes.tsx
+++ taskflow/frontend/src/routes/AppRoutes.tsx
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import NotFound from "../pages/NotFound";
+import Dashboard from "../pages/Dashboard";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
